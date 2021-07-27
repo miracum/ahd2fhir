cd13bd57510c97008c1ed1db2ee60c4faad5bf22
uklft <35134759+uklft@users.noreply.github.com>
2021-07-27 16:48:46 +0200

    Revert "Fix kidneystone and smoking status mappers"

--- ahd2fhir/mappers/ahd_to_observation_smkstat.py
+++ ahd2fhir/mappers/ahd_to_observation_smkstat.py
@@ -116,4 +116,5 @@ def get_smoking_status_observation_from_annotation(
     # value_quantity.code = "401201003"
     # value_quantity.text = "401201003"
     # observation.valueQuantity = value_quantity
-    return [observation]
+
+    return observation
