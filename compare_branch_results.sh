32a4da482e15bd29a699cf43c0f7cab4f50c7869
thomczyk <fabian.thomczyk@uniklinik-freiburg.de>
2021-09-28 12:13:40 +0200

    chore: Remove test script compare_branch_results.sh

--- compare_branch_results.sh
+++
@@ -1,6 +0,0 @@
-for BRANCH_NAME in create-mapper-container-class master; do
-  git checkout $BRANCH_NAME
-  curl -H "Content-Type: application/fhir+json" -d @tests/resources/fhir/documentreference.json http://localhost:8000/fhir/\$analyze-document | jq > /tmp/ahd2fhir_$BRANCH_NAME.json
-done
-
-diff /tmp/ahd2fhir_*.json
